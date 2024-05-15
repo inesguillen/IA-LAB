0b9d0336051cb725a771b8642f86c8473b410a14
Ines <inguipe@gmail.com>
2024-05-15 17:48:50 +0200

    final code

--- Source code.py
+++ Source code.py
@@ -28,8 +28,8 @@ def fuzzify(fuzzy_sets, data):
     for var, value in data:  # Loop through each variable-value
         # Find fuzzy sets corresponding to the variable
         for fuzzy_set_key, fuzzy_set in fuzzy_sets.items():
-            if fuzzy_set.var == var:
-                degree = skf.interp_membership(fuzzy_set.x, fuzzy_set.y, value)
+            if fuzzy_set.var == var:  # Check the variable is the one we want
+                degree = skf.interp_membership(fuzzy_set.x, fuzzy_set.y, value)  # Calculate degree
                 fuzzy_data[fuzzy_set_key] = degree  # Store the degree in the dictionary
     return fuzzy_data
 
@@ -63,6 +63,7 @@ def defuzzify(rule_strengths):
     max_degrees = [results['LowR'], results['MediumR'], results['HighR']]  # Array with the final max degrees values
     print("list max degree: ", max_degrees)
 
+    # We get the trapezoidal function graph for each risk
     low_risk = skf.trapmf(risks, [-20, -10, 30, 50])
     medium_risk = skf.trapmf(risks, [10, 40, 70, 90])
     high_risk = skf.trapmf(risks, [50, 70, 100, 111])
@@ -73,7 +74,6 @@ def defuzzify(rule_strengths):
         medium_risk * max_degrees[1],
         high_risk * max_degrees[2]
     ]
-
     combined_function = np.sum(weighted_functions, axis=0)  # Sum of all area
     centroid = skf.defuzz(risks, combined_function, 'centroid')  # Get centroid
     return centroid
@@ -84,7 +84,7 @@ fuzzySets = readFuzzySetsFile('Files/InputVarSets.txt')  # Read the fuzzy sets
 rulesList = readRulesFile()  # Read Inference Rules
 application = readApplicationsFile()  # Read Loan Applications
 
-result = []
+result = []  # Empty list to store the results
 for app in application:
     fuzzyData = fuzzify(fuzzySets, app.data)  # Fuzzify the input data
     rule_degrees = apply_rules(rulesList, fuzzyData)  # Apply the inference rules
@@ -92,7 +92,6 @@ for app in application:
     result.append((app.appId, final_centroid))  # Store the result (application ID and risk score)
     plot_degree_function(fuzzySets, fuzzyData)  # Show graph of each variable for each application
 
-# Write results to a file
-with open("Files/Results.txt", "w") as output_file:
+with open("Files/Results.txt", "w") as output_file:  # Write results to a file
     for app_id, risk in result:
         output_file.write(f"{app_id}, {risk:.2f}\n")
